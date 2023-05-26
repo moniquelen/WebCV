55d5d2c34f6435a7618dcd7e57f21edeeb94abdd
Monique Elen <moniquelen10@hotmail.com>
2023-05-25 23:54:33 -0300

    ajustes header + atualizacoes

--- script.js
+++ script.js
@@ -2,30 +2,30 @@ var el = document.querySelector("#text");
 var text = "Olá! Me chamo Monique Elen.";
 var interval = 80;
 function showtext(el, text, interval) {
-  var char = text.split("").reverse();
-  var typer = setInterval(function () {
-    if (!char.length) {
-      return clearInterval(typer);
-    }
-    var next = char.pop();
-    el.innerHTML += next;
-  }, interval);
+    var char = text.split("").reverse();
+    var typer = setInterval(function () {
+        if (!char.length) {
+            return clearInterval(typer);
+        }
+        var next = char.pop();
+        el.innerHTML += next;
+    }, interval);
 }
 showtext(el, text, interval);
 
 function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
-  function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute("href");
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute("href");
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    }
+    linksInternos.forEach((link) => {
+        link.addEventListener("click", scrollToSection);
     });
-  }
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
 }
 initScrollSuave();
